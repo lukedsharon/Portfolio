43bd3b66ee3e75e723771d00ddabfae61c0df9c8
Luke Sharon <lukedsharon@gmail.com>
2023-03-06 14:05:37 -0600

    Added experience object.

---
+++ src/components/base/ExperienceCard.tsx
@@ -0,0 +1,99 @@
+const EXPERIENCE = [
+  {
+    superTitle: "Education",
+    company: "College of the Ozarks",
+    position: "Point Lookout, MO",
+    startDate: "May, 2017",
+    endDate: "December, 2021",
+    description: [
+      {
+        title: "Computer Information Science",
+        description: "GPA: 3.8/4.0",
+      },
+      {
+        title: "Accounting",
+        description: "GPA: 3.8/4.0",
+      },
+    ],
+  },
+  {
+    superTitle: "Business Office",
+    company: "College of the Ozarks",
+    position: "3 yrs 8 mos",
+    startDate: "",
+    endDate: "",
+    description: [
+      {
+        title: "Balanced daily cash flow and prepared daily bank deposits",
+      },
+      {
+        title: "Processed account transactions",
+      },
+      {
+        title:
+          "Interpreted student account information and communicated details clearly",
+      },
+    ],
+  },
+  {
+    superTitle: "Technical Finance Developer",
+    company: "Classy Llama, LLC.",
+    position: "1 yr 4 mos",
+    startDate: "",
+    endDate: "",
+    description: [
+      {
+        title:
+          "Built and maintained system integrations with Salesforce, Quickbooks, Atlassian, and Google",
+      },
+      {
+        title: "Developed automations using REST APIs and the Celigo platform",
+      },
+      {
+        title:
+          "Customized Salesforce functionality using formulas, flows, and the Apex programming language",
+      },
+      {
+        title:
+          "Built and customized financial tools using Google Sheets and Apps Script for the Finance department",
+      },
+      {
+        title: "Wrote user stories and acceptance criteria for coding projects",
+      },
+    ],
+  },
+  {
+    superTitle: "Full Stack Developer",
+    company: "ConGenius, LLC.",
+    position: "6 mos",
+    startDate: "",
+    endDate: "",
+    description: [
+      {
+        title:
+          "Built front-end components based on UI designs and functional specifications",
+      },
+      {
+        title:
+          "Implemented font-end and back-end logic to ensure proper functionality",
+      },
+      {
+        title:
+          "Solved bugs and errors to align the website with expected functionality",
+      },
+      {
+        title:
+          "Performed modifications and additions to database code for proper data persistence, data integrity, and security",
+      },
+      {
+        title:
+          "Collaborated with team members to gain and provide assistance on relevant tasks",
+      },
+    ],
+  },
+];
+function ExperienceCard() {
+  return <div>ExperienceCard</div>;
+}
+
+export default ExperienceCard;
